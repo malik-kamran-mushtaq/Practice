99106cd4af7390014798abddab08c15d01887b57
Kamran <malikkmaranmushtaq@Maliks-MacBook-Air.local>
2023-05-29 00:13:39 +0530

    My first commit.Ading a new java file to the repo.

---
+++ ABC/A.java
@@ -0,0 +1,12 @@
+package Practice.ABC;
+
+public class A {
+    public static void main(String[] args) {
+        System.out.println("Learning git...");
+        int a=10;
+        int b=5;
+        int c=a+b;
+        System.out.println("Sum of "+a+" and "+b+" = "+c);
+    }
+    
+}
